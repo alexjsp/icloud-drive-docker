ac047dbdd9b7f6b1a2ea23413eca308787400676
Alex Stevenson-Price <alex@psmail.co>
2024-06-18 13:52:45 +0100

    Experimental change to try and fix unraid

--- init.sh
+++ init.sh
@@ -10,8 +10,8 @@ https://www.buymeacoffee.com/mandarons
 ====================================================
 '
 echo "Using UID as ${PUID}, GID as ${PGID} and UMASK as ${UMASK}..."
-addgroup --gid $PGID icd;
-adduser -D --uid $PUID icd -G icd; \
+groupadd -f --g $PGID icd;
+useradd -D --uid $PUID icd -G icd; \
 echo "icd ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/icd && sudo chmod 0440 /etc/sudoers.d/icd
 echo "Changing ownership of /app ... This operation may take significantly longer depending on number of files in your local copy of icloud."
 chown -R icd:icd /app
